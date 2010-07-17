7864835538fb1eee6cee9c0685d4c8a67946c742
Fernando Ferreira <fferreira@icsystems.com.br>
2010-07-17 15:15:14 -0300

    v1.3.8 Inseridas as sugestões da Daniela - Data consulta, numero do paciente e unidade de saude. Alguns campos modificados.

--- js/action.js
+++ js/action.js
@@ -1,3 +1,44 @@
+
+(function($){
+	$.fn.writePortugueseDate = function(){
+		var element = $(this[0]);
+		var mydate=new Date()
+		var year=mydate.getYear()
+		if (year<2000)
+		year += (year < 1900) ? 1900 : 0
+		var day=mydate.getDay()
+		var month=mydate.getMonth()
+		var daym=mydate.getDate()
+		if (daym<10)
+		daym="0"+daym
+		var dayarray=new Array(
+			"Domingo",
+			"Segunda-feira",
+			"Terça-feira",
+			"Quarta-feira",
+			"Quinta-feira",
+			"Sexta-feira",
+			"Sábado"
+		);
+		var montharray=new Array(
+			"de Janeiro de ",
+			"de Fevereiro de ",
+			"de Março de ",
+			"de Abril de ",
+			"de Maio de ",
+			"de Junho de",
+			"de Julho de ",
+			"de Agosto de ",
+			"de Setembro de ",
+			"de Outubro de ",
+			"de Novembro de ",
+			"de Dezembro de "
+		);
+		var msg = dayarray[day]+", "+daym+" "+montharray[month]+year;
+		element.val(msg);
+	};
+})(jQuery);
+
 function argumentsNNet(){
 	this.idade;
 	this.tosse;
@@ -152,6 +193,7 @@ $(document).ready(function(){
 		}
 	});
 
+	$('#data_consulta').writePortugueseDate();
 	//Build birthday calendar
 	$('#data_nascimento').datepicker({
 			dateFormat: 'dd/mm/yy',
